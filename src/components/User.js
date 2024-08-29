9a92c77bfc45d4f40c4aeb96074f55d5aa129dcd
chirag wankhede <chirag20wankhede@gmail.com>
2024-08-29 16:15:17 +0530

    final commit

---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User=()=>{
+    return(
+        <div className="user-card">
+            <h2>Name : Chirag</h2>
+            <h3>Location: Pune</h3>
+            <h4>Contact: @chirag20wankhede</h4>
+        </div>
+    )
+}
+
+export default User;
---
+++ src/utils/UserContext.js
@@ -0,0 +1,7 @@
+import { createContext} from "react";
+
+const UserContext=createContext({
+    loggedInUser:"Chirag Wankhede"
+});
+
+export default UserContext;
